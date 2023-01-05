112bb5dd7db8675e0c9afee846ead91e67aa31df
“Spencer <srye1140@gmail.com>
2023-01-04 17:52:57 -0800

    Adds functional routes for products, minor syntax corrections on category-routes.js

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -9,7 +9,7 @@ router.get('/', async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
-      include: [{ model: Product}]
+      include: [{ model: Product}],
     });
     res.status(200).json(categoryData);
   } catch(err) {
@@ -22,7 +22,7 @@ router.get('/:id', async (req, res) => {
   // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
-      include: [{model: Product}]
+      include: [{model: Product}],
     });
 
     if (!categoryData) {
